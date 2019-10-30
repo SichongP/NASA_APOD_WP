381ef26f25c86daceff92c09e2bc514be8e3e645
SichongP <scpeng@ucdavis.edu>
2019-10-29 17:23:57 -0700

    script to auto set wallpaper as NASA APOD

---
+++ NASA_APOD_WP/NASA_APOD_WP.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/python3
+
+import requests, datetime, urllib, pathlib, os
+
+img_folder = str(pathlib.Path.home()) + "/NASA_APOD/"
+if not os.path.exists(img_folder):
+    os.makedirs(img_folder)
+date = str(datetime.datetime.date(datetime.datetime.today()))
+if os.path.exists(os.path.join(img_folder, date + ".jpg")):
+    exit(0)
+metadata = requests.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+file_path, header = urllib.request.urlretrieve(metadata.json()['hdurl'], img_folder + date + ".jpg")
+os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri file://" + file_path)
+
+
