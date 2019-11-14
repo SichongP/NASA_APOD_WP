8fe6f028b041b7609f7162ea915bf975ecd6daed
SichongP <scpeng@ucdavis.edu>
2019-11-14 11:14:50 -0800

    Add new line to print description of the photo

--- NASA_APOD_WP/NASA_APOD_WP.py
+++ NASA_APOD_WP/NASA_APOD_WP.py
@@ -9,6 +9,7 @@ date = str(datetime.datetime.date(datetime.datetime.today()))
 if os.path.exists(os.path.join(img_folder, date + ".jpg")):
     exit(0)
 metadata = requests.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+print(metadata.json()['explanation'])
 file_path, header = urllib.request.urlretrieve(metadata.json()['hdurl'], img_folder + date + ".jpg")
 os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri file://" + file_path)
 
