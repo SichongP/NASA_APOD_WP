a7c84462caacd9bfe40c935281314407c2e37ab4
SichongP <scpeng@ucdavis.edu>
2019-11-18 10:05:29 -0800

    Update to handle when image of the day is a video

--- NASA_APOD_WP/NASA_APOD_WP.py
+++ NASA_APOD_WP/NASA_APOD_WP.py
@@ -10,6 +10,10 @@ if os.path.exists(os.path.join(img_folder, date + ".jpg")):
     exit(0)
 metadata = requests.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 print(metadata.json()['explanation'])
+if metadata.json()['media_type'] == 'video':
+    print("Today's selection is a video. Check it out here: \n{}".format(metadata.json()["url"]))
+    os.system("touch {}".format(img_folder + date + ".jpg"))
+    exit(0)
 file_path, header = urllib.request.urlretrieve(metadata.json()['hdurl'], img_folder + date + ".jpg")
 os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri file://" + file_path)
 
