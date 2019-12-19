be21156fdf9a91afc396045f16254e90449d935b
SichongP <scpeng@ucdavis.edu>
2019-12-19 11:41:04 -0800

    Save description to a txt file with same name for daily pics

--- NASA_APOD_WP/NASA_APOD_WP.py
+++ NASA_APOD_WP/NASA_APOD_WP.py
@@ -9,6 +9,8 @@ date = str(datetime.datetime.date(datetime.datetime.today()))
 if os.path.exists(os.path.join(img_folder, date + ".jpg")):
     exit(0)
 metadata = requests.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+with open(os.path.join(img_folder, date + ".txt"), 'w') as info_txt:
+    info_txt.write(metadata.json()['explanation'])
 print(metadata.json()['explanation'])
 if metadata.json()['media_type'] == 'video':
     print("Today's selection is a video. Check it out here: \n{}".format(metadata.json()["url"]))
